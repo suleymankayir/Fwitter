d610c05baabb82937fd74fa94307cadf1bb65f88
suleymankayir <suleymantalhakayir@gmail.com>
2024-03-13 00:39:35 +0300

    user and role tables created commit

--- fwitter-backend/src/main/java/com/fwitter/models/ApplicationUser.java
+++ fwitter-backend/src/main/java/com/fwitter/models/ApplicationUser.java
@@ -36,8 +36,8 @@ public class ApplicationUser {
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_role_junction",
-            joinColumns = {@JoinColumn(name="user_id")},
-            inverseJoinColumns = {@JoinColumn(name="role_id")}
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")}
     )
     private Set<Role> authorities;
 
