630ff4fb3084e231630706089520f482e49b1320
suleymankayir <suleymantalhakayir@gmail.com>
2024-03-13 00:38:05 +0300

    first commit

---
+++ fwitter-backend/src/main/java/com/fwitter/FwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwitterBackendApplication.class, args);
+	}
+
+}
